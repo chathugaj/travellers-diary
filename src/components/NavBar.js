8a9381829e7a1bc1e2dae411942963c86bac1035
Chathurika Gajanayaka <chathurikagajanayaka@chathurikas-air.home>
2024-05-07 02:40:34 +0200

    Add css styles to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,20 +1,40 @@
 import React from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import NavbarCollapse from "react-bootstrap/esm/NavbarCollapse";
+import styles from "../styles/NavBar.module.css";
 
 const NavBar = () => {
   return (
-    <Navbar collapseOnSelect fixed="top" bg="dark" variant="dark" expand="lg">
+    <Navbar
+      className={styles.NavBar}
+      collapseOnSelect
+      fixed="top"
+      bg="dark"
+      variant="dark"
+      expand="lg"
+    >
       <Container>
-        <Navbar.Brand href="#">TRAVELER'S DIARY</Navbar.Brand>
+        <Navbar.Brand className={styles.NavTitle} href="#">
+          TRAVELER'S DIARY
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll" className="justify-content-end">
           <Nav className="ml-auto my-2 my-lg-0" navbarScroll>
-            <Nav.Link href="#action1">HOME</Nav.Link>
-            <Nav.Link href="#action2">CONTACT US</Nav.Link>
-            <Nav.Link href="#action3">BLOG POST</Nav.Link>
-            <Nav.Link href="#action4">SIGN IN</Nav.Link>
-            <Nav.Link href="#action5">SIGN UP</Nav.Link>
+            <Nav.Link className={styles.NavLink} href="#action1">
+              HOME
+            </Nav.Link>
+            <Nav.Link className={styles.NavLink} href="#action2">
+              CONTACT US
+            </Nav.Link>
+            <Nav.Link className={styles.NavLink} href="#action3">
+              BLOG POST
+            </Nav.Link>
+            <Nav.Link className={styles.NavLink} href="#action4">
+              SIGN IN
+            </Nav.Link>
+            <Nav.Link className={styles.NavLink} href="#action5">
+              SIGN UP
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
