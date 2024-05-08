28fe231b90a78295924f0ec4739c57e19d5ed76d
Chathurika Gajanayaka <chathurikagajanayaka@chathurikas-air.home>
2024-05-08 02:05:00 +0200

    Add social media links to the footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -11,20 +11,44 @@ const Footer = () => {
             <ul className={`${styles.ListInline} ${styles.TextCenter}`}>
               <li class="list-inline-item">
                 <span class="fa-stack fa-lg">
-                  <i class="fa fa-circle fa-stack-2x"></i>
-                  <i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
+                  <a
+                    href="https://facebook.com"
+                    target="_blank"
+                    rel="noreferrer"
+                    aria-label="visit our facebook page(opens in a new tab)"
+                    className={styles.SocialLink}
+                  >
+                    <i class="fa fa-circle fa-stack-2x"></i>
+                    <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
+                  </a>
                 </span>
               </li>
               <li class="list-inline-item">
                 <span class="fa-stack fa-lg">
-                  <i class="fa fa-circle fa-stack-2x"></i>
-                  <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
+                  <a
+                    href="https://twitter.com"
+                    target="_blank"
+                    rel="noreferrer"
+                    aria-label="visit our twitter page(opens in a new tab)"
+                    className={styles.SocialLink}
+                  >
+                    <i class="fa fa-circle fa-stack-2x"></i>
+                    <i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
+                  </a>
                 </span>
               </li>
               <li class="list-inline-item">
                 <span class="fa-stack fa-lg">
-                  <i class="fa fa-circle fa-stack-2x"></i>
-                  <i class="fa fa-github fa-stack-1x fa-inverse"></i>
+                  <a
+                    href="https://instagram.com"
+                    target="_blank"
+                    rel="noreferrer"
+                    aria-label="visit our instagram page(opens in a new tab)"
+                    className={styles.SocialLink}
+                  >
+                    <i class="fa fa-circle fa-stack-2x"></i>
+                    <i class="fa fa-instagram fa-stack-1x fa-inverse"></i>
+                  </a>
                 </span>
               </li>
             </ul>
