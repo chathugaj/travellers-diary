2e383113174a41b7cf2d3d38b36a183ec0ddc0b2
Chathurika Gajanayaka <chathurikagajanayaka@chathurikas-air.home>
2024-07-08 11:51:09 +0200

    formatting fix

--- src/pages/post/PostContent.js
+++ src/pages/post/PostContent.js
@@ -1,12 +1,14 @@
-import {Col, Row} from "react-bootstrap";
+import { Col, Row } from "react-bootstrap";
 import React from "react";
 
 const PostContent = ({ post }) => {
-    return <Row>
-        <Col md={10} lg={8} className="mx-auto">
-            <p>{ post?.body }</p>
-        </Col>
-    </Row>;
-}
+  return (
+    <Row>
+      <Col md={10} lg={8} className="mx-auto">
+        <p>{post?.body}</p>
+      </Col>
+    </Row>
+  );
+};
 
 export default PostContent;
